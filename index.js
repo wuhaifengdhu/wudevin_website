86ff0d483f070bd82518e71ba188dc2bc1f65d31
wuhaifengdhu <wuhaifengdhu@163.com>
2018-01-06 21:06:48 +0800

    clean unused files

--- index.js
+++ index.js
@@ -10,47 +10,43 @@ app.use(cookieParser()) ;
 app.use(bodyParse.urlencoded({extended:false})) ;
 app.engine('html', ejs.renderFile);
 app.set("view engine", "html");
+// 在生产环境，你应该将此处的 store 换为某种永久存储。
+// 请参考 http://expressjs.com/2x/guide.html#session-support
 // 如果需要 session 支持，sessionStore 必须放在 watch 之后
 app.use(expressSession({
-    secret: 'abcd123',
+    secret: 'someElse',
     store: new expressSession.MemoryStore(),
     resave: false,
     saveUninitialized: true
 }));
 
-
-app.get('/gan',function(req,res){
-    res.sendfile('gan.html');
-    console.log('gan page is required ');
-});
-
-app.get('/me',function(req,res){
-    res.sendfile('home.html');
-    console.log('main page is required ');
-});
-
-app.get('/red/:userid',function(req,res){
+function generate_user_page(res, userid){
     new_url = "https://qr.alipay.com/" + req.params.userid;
     res.render("template", {new_url : new_url});
     console.log('redirect new url is  ' + new_url);
+}
+
+app.get('/gan',function(req,res){
+    generate_user_page(res, "c1x04314waex22zanlx2u57");
+});
+
+app.get('/me',function(req,res){
+    generate_user_page(res, "c1x08530scdkzjtabmt7f27");
 });
 
 app.get('/wuya',function(req,res){
-    res.sendfile('wuya.html') ;
-    console.log('wuya page is required ');
+    generate_user_page(res, "c1x00712pbzcldyrqgyqj03");
+});
+
+app.get('/red/:userid',function(req,res){
+    generate_user_page(res, req.params.userid);
 });
 
 new Wolf().monitor(app, 'wolf', '/wechat');
 
-// 在生产环境，你应该将此处的 store 换为某种永久存储。
-// 请参考 http://expressjs.com/2x/guide.html#session-support
 // 在环境变量提供的 $PORT 或 3000 端口监听
 var port = process.env.PORT || 80
 app.listen(port, function(){
     console.log("Listening on %s", port);
 })
-
-// 微信接口地址只允许服务放在 80 端口
-// 所以需要做一层 proxy
-//app.enable('trust proxy');
 
